0be65f82efbbc9883ecacebc838241d05a85e789
Dejan Stojadinovic <dejanstojadinovic@outlook.com>
2018-02-27 01:49:26 +0100

    removed redux devtools

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -2,12 +2,13 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers/index';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose ;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose ;
 
 export default () => {
   const store = createStore(
     combineReducers(reducers),
-    composeEnhancers(applyMiddleware(thunk))
+    // composeEnhancers(applyMiddleware(thunk))
+    applyMiddleware(thunk)
   );
 
   return store;
