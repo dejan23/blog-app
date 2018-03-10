d0984b257cf7600157df853a4c352c9f5b71d8a1
Dejan Stojadinovic <dejanstojadinovic@outlook.com>
2018-03-10 15:37:55 +0100

    f

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-require('dotenv').config();
 
 module.exports = env => {
   const isProduction = env === 'production';
