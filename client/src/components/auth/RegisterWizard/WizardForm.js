5eb3bf56ac872504ade1195ef7d6dbcc2d02a058
Dejan Stojadinovic <dejanstojadinovic@outlook.com>
2018-04-07 19:15:57 +0200

    removed forgotten import

--- client/src/components/auth/RegisterWizard/WizardForm.js
+++ client/src/components/auth/RegisterWizard/WizardForm.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import WizardFormFirstPage from './WizardFormFirstPage'
 import WizardFormSecondPage from './WizardFormSecondPage'
-import WizardFormThirdPage from './WizardFormThirdPage'
 
 class WizardForm extends Component {
   constructor(props) {
